66b3259af41eb8dbf9c99d9ce9e79820b5e81678
naehyun25 <yoon658@naver.com>
2023-05-15 13:11:00 +0900

    Composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -8,6 +8,8 @@ const ExpenseItem = (props)=>{
     //toISOString
     return(
         <Card className="expense-item">
+            {/* Card 컴포넌트는 div 가 아니어서 현재 className 적용이되지않는걸 확인할 수 있다. */}
+            {/* 그래서 css 도 props 로 처리한다.=>Card 파일 확인 */}
             <ExpenseDate date={date}/>
             <div className="expense-item__description">
                 <h2>{title}</h2>
--- src/ui/Card.js
+++ src/ui/Card.js
@@ -1,8 +1,11 @@
 import "./Card.css";
+//반복되는 디자인 ui 만들기(데이터전달 : props.children + ui => composition)
 const Card = (props) => {
-    console.log(props.children)
+    const classes=`card ${props.className}`;
+    console.log("card",props) // className 도 props로 들어온다.
     return(
-        <div className="card">
+        <div className={classes}>
+            {props.children}
         </div>
     )
 };
